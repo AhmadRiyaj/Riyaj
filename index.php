ddcf81a0f2676cf2cd56abcc1d98a385202be116
AhmadRiyaj <rs10052001@gmail.com>
2021-09-26 23:26:52 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index1.php" method="post" enctype="multipart/form-data">
+    <div>
+       Name <input type="text" name="name" id=""><br>
+        Email<input type="text" name="email" id=""><br>
+        Phone Number<input type="text" name="number" id=""><br>
+        Password<input type="password" name="password" id=""><br>
+        <input type="submit" name="submit" id="">
+    </div>
+
+
+</form>
+    
+</body>
+</html>
